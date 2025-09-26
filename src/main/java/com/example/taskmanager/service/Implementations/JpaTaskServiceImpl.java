76ed32277732136df076727f9e2732640d3c8733
niko <nikolaevsasha3@gmail.com>
2025-09-26 17:08:03 +0300

    add profiles

---
+++ src/main/java/com/example/taskmanager/service/Implementations/JpaTaskServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.taskmanager.service.Implementations;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.service.TaskService;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import com.example.taskmanager.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+@Profile("h2")
+public class JpaTaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Override
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public List<Task> getUserTasks(Long userId) {
+    return taskRepository.findByUserId(userId);
+}
+
+    @Override
+    public List<Task> getPendingTasks(Long userId) {
+        return taskRepository.findByUserIdAndIsDoneFalse(userId).stream()
+                .filter(task -> !task.getIsDeleted())
+                .toList();
+    }
+
+    @Override
+    public boolean deleteTask(Long taskId) {
+        Optional<Task> optionalTask = taskRepository.findById(taskId);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            if (!task.getIsDeleted()) {
+                task.setIsDeleted(true);
+                taskRepository.save(task);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Task> getTaskById(Long taskId) {
+        return taskRepository.findById(taskId);
+    }
+}
---
+++ src/main/java/com/example/taskmanager/service/Implementations/JpaUserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.taskmanager.service.Implementations;
+
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.service.UserService;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import com.example.taskmanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+@Profile("h2")
+public class JpaUserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User registerUser(String username, String password) {
+        User user = User.builder()
+                .username(username)
+                .password(password)
+                .build();
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> login(String username, String password) {
+        return userRepository.findByUsername(username)
+                .filter(user -> user.getPassword().equals(password));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
